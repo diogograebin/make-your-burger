c3363dd1b60a6dc579d303f2a25629144ef4595a
Diogo Grings Graebin <diogograebin@gmail.com>
2025-02-25 21:37:02 -0300

    implementando o vue router

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  
 </template>
 
 <script>
@@ -10,9 +7,6 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
