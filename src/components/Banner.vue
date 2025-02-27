91dd1e879a1fd11ff7f371e10a1801942e3a2b86
Diogo Grings Graebin <diogograebin@gmail.com>
2025-02-26 22:03:05 -0300

    finalização navbar e footer

---
+++ src/components/Banner.vue
@@ -0,0 +1,11 @@
+<template>
+    <p>burger-banner</p>
+</template>
+<script>
+export default{
+
+}
+</script>
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="Home">
-    <h1>HOME</h1>
+  <Banner />
+  <div class="main-container">
+    <h1>Monte seu Burger:</h1>
+    <p>form</p>
   </div>
 </template>
 
 <script>
 
-export default {
+import Banner from "../components/Banner.vue"
 
+export default {
+  name: "Home",
+  components: {
+    Banner,
+  }
 }
 </script>
