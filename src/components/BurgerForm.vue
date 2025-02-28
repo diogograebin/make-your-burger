fd7e4990a7024b1f21657e9c3ba3de4372a2ae8e
Diogo Grings Graebin <diogograebin@gmail.com>
2025-02-27 23:43:29 -0300

    primeira versão do estilo do formulário

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -1,15 +1,14 @@
 <template>
     <div>
-        <p>Componente de Mensagem</p>
         <div>
             <form id="burger-form">
-                <div class="imput-container">
+                <div class="input-container">
                     <label for="nome">Nome do Cliente:</label>
                     <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite o seu nome">
                     
                 </div>
 
-                <div class="imput-container">
+                <div class="input-container">
                     <label for="pao">Escolha o Pão:</label>
                     <select name="pao" id="pao" v-model="pao">
                         <option value="">Selecione seu pão</option>
@@ -17,7 +16,7 @@
                     </select>
                 </div>
 
-                <div class="imput-container">
+                <div class="input-container">
                     <label for="carne">Escolha a Carne do seu Burger:</label>
                     <select name="carne" id="carne" v-model="carne">
                         <option value="">Selecione o tipo de carne</option>
@@ -25,8 +24,8 @@
                     </select>
                 </div>
 
-                <div class="imput-container">
-                    <label for="opcionais">Selecione os Opcionais:</label>
+                <div id="opcionais-container" class="input-container">
+                    <label id="opcionais-title" for="opcionais">Selecione os Opcionais:</label>
                     <div class="checkbox-container">
                         <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
                         <span>Salame</span>
@@ -50,6 +49,72 @@ export default{
 </script>
 
 <style scoped>
+    #burger-form {
+        max-width: 400px;
+        margin: 0 auto;
+    }
+
+    .input-container{
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 20px;
+    }
+
+    label{
+        font-weight: bold;
+        margin-bottom: 15px;
+        color: #222;
+        padding: 5px 10px;
+        border-left: 4px solid #fcba03;
+    }
+
+    input, select{
+        padding: 5px 10px;
+        width: 300px;
+    }
+
+    #opcionais-container{
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+
+    #opcionais-title{
+        width: 100%;
+    }
+
+    .checkbox-container{
+        display: flex;
+        align-items: center;
+        width: 50%;
+        margin-bottom: 20px;
+    }
+
+    .checkbox-container span, .checkbox-container input{
+        width: auto;
+    }
+
+    .checkbox-container span{
+        margin-left: 6px;
+        font-weight: bold;
+    }
+
+    .submit-btn{
+        background-color: #222;
+        color: #fcba03;
+        font-weight: bold;
+        border: 2px solid #222;
+        padding: 10px;
+        font-size: 16px;
+        margin: 0 auto;
+        cursor: pointer;
+        transition: .5s;
+    }
+
+    .submit-btn:hover{
+        background-color: transparent;
+        color: #222;
+    }
+
 
 
 </style>
