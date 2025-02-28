050bbaba74e056a8cbed391a98809da54faf40c6
Diogo Grings Graebin <diogograebin@gmail.com>
2025-02-27 22:26:28 -0300

    primeira configuração frontend homeview

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@ export default{
 
 </script>
 
-<style>
+<style> 
   *{
     font-family: Helvetica;
     padding: 0;
--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,11 +1,29 @@
 <template>
-    <p>burger-banner</p>
+    <div id="main-banner" :style="{ backgroundImage: `url('/img/burger.jpg')` }">
+        <h1>Make Your Burger</h1>
+    </div>
 </template>
 <script>
 export default{
-
+    name: "Banner"
 }
 </script>
 <style scoped>
+    #main-banner{
+        background-position: 0 -250px;
+        background-size: cover;
+        height: 500px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+    }
 
+    #main-banner h1{
+        color: #fff;
+        text-align: center;
+        font-size: 60px;
+        background-color: #222;
+        padding: 20px 40px;
+    }
+    
 </style>
