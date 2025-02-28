c9ba1a90aeb8fd4daf109fe2e5168ec1cbf54635
Diogo Grings Graebin <diogograebin@gmail.com>
2025-02-27 23:15:09 -0300

    configuração inicial da estrutura de BurgerForm

---
+++ src/components/BurgerForm.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <p>Componente de Mensagem</p>
+        <div>
+            <form id="burger-form">
+                <div class="imput-container">
+                    <label for="nome">Nome do Cliente:</label>
+                    <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite o seu nome">
+                    
+                </div>
+
+                <div class="imput-container">
+                    <label for="pao">Escolha o Pão:</label>
+                    <select name="pao" id="pao" v-model="pao">
+                        <option value="">Selecione seu pão</option>
+                        <option value="integral">Integral</option>
+                    </select>
+                </div>
+
+                <div class="imput-container">
+                    <label for="carne">Escolha a Carne do seu Burger:</label>
+                    <select name="carne" id="carne" v-model="carne">
+                        <option value="">Selecione o tipo de carne</option>
+                        <option value="maminha">Maminha</option>
+                    </select>
+                </div>
+
+                <div class="imput-container">
+                    <label for="opcionais">Selecione os Opcionais:</label>
+                    <div class="checkbox-container">
+                        <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
+                        <span>Salame</span>
+                    </div>
+                </div>
+
+                <div class="input-container">
+                    <input type="submit" class="submit-btn" value="Criar meu Burger">
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default{
+    name: "BurgerForm"
+}
+
+</script>
+
+<style scoped>
+
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,20 @@
   <Banner />
   <div class="main-container">
     <h1>Monte seu Burger:</h1>
-    <p>form</p>
+    <BurgerForm />
   </div>
 </template>
 
 <script>
 
-import Banner from "../components/Banner.vue"
+import Banner from "../components/Banner.vue";
+import BurgerForm from "@/components/BurgerForm.vue";
 
 export default {
   name: "Home",
   components: {
     Banner,
+    BurgerForm
   }
 }
 </script>
